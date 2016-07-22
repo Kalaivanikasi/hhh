5a7d189a09539fda89b95e24658090b6039ba552
Kalaivani <kalaivani.kcse@ksrce.ac.in>
2016-07-22 09:59:12 +0530

    Create PositiveOrNegative.java

---
+++ PositiveOrNegative.java
@@ -0,0 +1,13 @@
+public class positive or negative
+{
+int num;
+System.out.println("enter the num");
+if(n>0)
+{
+System.out.println("num is positive");
+}
+else
+{
+System.out.println("num is negative");
+}
+}
